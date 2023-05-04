9bcde883486cb2775ab6e942b5a8e61e4be946fc
Ryan Nelson <ryiscool247@gmail.com>
2023-05-04 16:27:16 -0400

    Initial game state implementation and moved message log

--- src/main/java/org/zk/dataClasses/GameState.java
+++ src/main/java/org/zk/dataClasses/GameState.java
@@ -1,7 +1,41 @@
 package org.zk.dataClasses;
 
+import java.util.ArrayList;
+
 public class GameState extends ZookeeperData{
     //Convening with Tom tomorrow before designing this!
+
+    ArrayList<Player> players;
+    int currentPlayer;
+    String actionMessage;
+
+    public GameState(int[] playerTypes, int currentPlayer, String actionMessage) {
+        this.players = new ArrayList<>();
+        this.currentPlayer = currentPlayer;
+        this.actionMessage = actionMessage;
+
+        for (int type : playerTypes) {
+            this.players.add(new Player(type));
+        }
+    }
+
+    public void attack() {
+        Player player = players.get(currentPlayer);
+        Player villain = players.get(3);
+
+        actionMessage = player.attack(villain);
+    }
+
+    public void defend() {
+        Player player = players.get(currentPlayer);
+        actionMessage = player.defend();
+    }
+
+    public void reload() {
+        Player player = players.get(currentPlayer);
+        actionMessage = player.reload();
+    }
+
     @Override
     public byte[] serialize() {
         return new byte[0];
