caa6286ae4279995e0fa89934ed5208bf820367e
daltamur <daltamur@oswego.edu>
2023-05-14 17:15:13 -0400

    added print statement

--- src/main/java/org/server/RequestHandler.java
+++ src/main/java/org/server/RequestHandler.java
@@ -86,7 +86,7 @@ public class RequestHandler implements Runnable{
         int port_num;
         switch ((int) data.get(0)){
             case 20:
-//                System.out.println("Received an initial awake connection from " + client);
+                System.out.println("Received an initial awake connection from " + client);
                 GameRoomsInfo gfInfo = Main.zkClient.getGameRoomsInfo();
                 port_num = data.getInt(1);
                 channel.send(new Factory().makeGameRooms(new int[]{gfInfo.getGameRoom(0).getSize(),gfInfo.getGameRoom(1).getSize(),gfInfo.getGameRoom(2).getSize()}, new boolean[]{gfInfo.getGameRoom(0).getRoomIsFull(), gfInfo.getGameRoom(1).getRoomIsFull(), gfInfo.getGameRoom(2).getRoomIsFull()},new int[]{gfInfo.getGameRoom(0).getState(),gfInfo.getGameRoom(1).getSize(),gfInfo.getGameRoom(2).getSize()}, new int[]{Main.zkClient.checkServerStatus("rho.cs.oswego.edu"),Main.zkClient.checkServerStatus("moxie.cs.oswego.edu"),Main.zkClient.checkServerStatus("altair.cs.oswego.edu")},Main.zkClient.getPlayerCount()), client);
